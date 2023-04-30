1862eb9eb647d6f0545535d0bc4c89c58f1f73ab
Andrew Dupont <github@andrewdupont.net>
2023-04-30 16:10:01 -0700

    Initial import

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    atom: true
+  },
+  rules: {
+    "space-before-function-paren": ["error", "always"],
+    "no-cond-assign": "off"
+  }
+};
